8f90ed263b7f6ad00d8d521fe1770580a5488621
siamshaeed <siamshaeed@gmail.com>
2021-02-21 16:31:48 -0800

    footer file

---
+++ footer.php
@@ -0,0 +1,29 @@
+		<div class="footer">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-md-6">
+						<p>&copy;All Right Reserved 2017</p>
+					</div>
+					<div class="col-md-6">
+						<p>Deloped by <a href="#">DIU</a></p>
+					</div>
+				</div>				
+			</div>
+		</div>	  
+	   </div><!-- end container -->
+
+<script src="js/jquery-1.12.0.min.js"></script>
+<script src="js/bootstrap.min.js"></script> 
+<!-- Include Date Range Picker -->
+<script type="text/javascript" src="http://cdn.jsdelivr.net/semantic-ui/1.2.0/semantic.min.js"></script>
+<script type="text/javascript" src="js/formValidation.js"></script>
+<script type="text/javascript" src="js/semantic.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
+
+<!-- <script src="js/plugins.js"></script> -->
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/jquery.slicknav.min.js"></script>
+<script type="text/javascript" src="js/jquery.fitvids.js" ></script>
+<script src="js/main.js"></script>
+</body>
+</html>
